03d72d388559e5525dee372bc7886ada4a88eb45
Cristian Greco <c.greco@strath.ac.uk>
2020-03-02 18:35:56 +0000

    CMakeLists updated

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*
+Code for adaptive reconstruction based on residual evaluation
+Input config file + error file (+ Modes,Coefs and Encontent RDMD if already available)
+
+Output reconstructed field at the desired time instants with the adaptive technique
+based on residual evaluation
+*/
+
+#include <iostream>
+
+
+int main( int argc, char *argv[] )
+{
+    std::cout << "main starts " << std::endl;
+
+    std::cout << "main ends " << std::endl;
+
+    return 0;
+}
